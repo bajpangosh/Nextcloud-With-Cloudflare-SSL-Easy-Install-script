858f7870ea124eaf14719629a14661d9781b0eb6
Bajpan Gosh <bajpangosh@gmail.com>
2018-10-25 14:40:49 +0530

    updated php

--- nc_fresh_install.sh
+++ nc_fresh_install.sh
@@ -47,7 +47,7 @@ sudo systemctl restart nginx.service
 echo "lets install php 7.0 and modules"
 sleep 2;
 sudo apt install php7.0 php7.0-fpm -y
-sudo apt-get -y install php7.0-fpm php7.0-curl php7.0-gd php7.0-imap php7.0-mcrypt php7.0-readline php7.0-common php7.0-recode php7.0-mysql php7.0-cli php7.0-curl php7.0-mbstring php7.0-bcmath php7.0-mysql php7.0-opcache php7.0-zip php7.0-xml php-memcached php-imagick php-memcache memcached graphviz php-pear php-xdebug php-msgpack  php7.0-soap
+sudo apt-get -y install php7.0-curl php7.0-intl php7.0-gd php7.0-imap php7.0-mcrypt php7.0-readline php7.0-common php7.0-recode php7.0-mysql php7.0-cli php7.0-curl php7.0-mbstring php7.0-bcmath php7.0-mysql php7.0-opcache php7.0-zip php7.0-xml php-memcached php-imagick php-memcache memcached graphviz php-pear php-xdebug php-msgpack  php7.0-soap
 
 echo "Some php.ini tweaks"
 sleep 2;
